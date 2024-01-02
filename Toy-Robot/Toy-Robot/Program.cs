b3116a11d51473a8b99803f28e533ae19dd87d52
Kitsune Starwind <lord_silver_fox@hotmail.com>
2024-01-03 09:57:32 +1000

    initial

---
+++ Toy-Robot/Toy-Robot/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Toy_Robot
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
